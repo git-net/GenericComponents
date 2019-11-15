86197e73fdf330fc6c276409cb0f569f3c3efd2a
myfp <lifnetbox@126.com>
2019-11-16 00:31:31 +0800

    init

---
+++ GenericComponentVS/Shared/case1/Case1Base.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GenericComponentVS.Shared
+{
+    public abstract  class  Case1Base<TComp>: ComponentBase
+    {
+       [Parameter] public RenderFragment<TComp> ChildContent { get; set; }
+
+     
+    }
+}
---
+++ GenericComponentVS/Shared/case2/Case2Base.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GenericComponentVS.Shared
+{
+    public abstract class Case2Base<TComp, TItem> : Case1Base<TComp> {
+ 
+        [Parameter] public RenderFragment<(TComp Comp, TItem obj )> Template { get; set; } //Test.razor  line3 cause  compile fail cs0234  
+        //[Parameter] public RenderFragment<Args<TComp,TItem>> Template { get; set; }// compile success , works.
+    }
+
+    public class Args<TComp, TItem>
+    {
+        public TComp Comp { get; set; }
+        public TItem Item { get; set; }
+    }
+}
